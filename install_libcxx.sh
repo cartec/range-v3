0e3b475b5ed369ab1264146f81dac2fe1f0b1cea
Casey Carter <Casey@Carter.net>
2017-08-24 08:24:08 -0700

    Travis updates:
    * change the llvm sources from XXX-precise-XXX to XXX-trusty-XXX
    
    * Don't cache cmake; downloading from cache provides no benefit over downloading from cmake.org
    
    * No configuration actually needs the util-linux package
    
    * Add clang 4 configurations
    
    * Travis refuses to whitelist the clang 3.7 trusty repo, so install 3.7 "by hand".
    
    * MUCH scripting cleanup / syntax simplification
    
    * Install libc++ into ${DEPS_DIR}/libcxx instead of /usr
    
    install_libcxx:
    
    * Accept an optional install prefix parameter (sets the stage for caching libc++ builds)
    
    * Accept an optional number-of-parallel tasks parameter "-j n"
    
    * install the most recent point release for a clang version unless fully specified
    
    * overlap downloading and unpacking

--- install_libcxx.sh
+++ install_libcxx.sh
@@ -1,99 +1,88 @@
 #!/usr/bin/env bash
 
-TRUNK_VERSION="5.0.0"
-
 set -e
 
-# The pattern of clang --version is: clang version X.Y.Z (sometimes, see below).
-COMPILER_VERSION_OUTPUT="$($CXX --version)"
-arr=(${COMPILER_VERSION_OUTPUT// / })
+TRUNK_VERSION=5
+TASKS=2
 
-COMPILER="${arr[0]}"
-VERSION="${arr[2]}"
+while getopts j: OPT; do
+    case "$OPT" in
+    j) TASKS=$OPTARG;;
+    ?) exit 1;;
+    esac
+done
 
-case $COMPILER in
-    "clang")
-        # Some Ubuntu clang builds are advertised as "just clang", but the
-        # Version still follows the pattern: 3.6.2-svn240577-1~exp1
-        # echo "Compiler is clang :)"
-        arr2=(${VERSION//-/ })
-        VERSION="${arr2[0]}"
-        ;;
-    "Ubuntu")
-        # Ubuntu renames _some_ (not all) of its clang compilers, the pattern of
-        # clang --version is then:
-        # Ubuntu clang version 3.6.2-svn240577-1~exp1
-        COMPILER="${arr[1]}"
-        VERSION="${arr[3]}"
-        arr2=(${VERSION//-/ })
-        VERSION="${arr2[0]}"
-        ;;
-    *)
-        echo "case did not match: compiler: ${COMPILER}"
-        exit 1
-        ;;
-esac
-
-if [ ${COMPILER} != "clang" ]; then
-    echo "Error: trying to install libc++ for a compiler that is not clang: ${COMPILER}"
+if [[ $OPTIND -gt $#  ]]; then
+    echo "Usage: $0 [-j N] VERSION [INSTALL_PREFIX]" 1>&2
     exit 1
 fi
 
-if [ -z ${VERSION+x} ]; then
-    echo "libc++ version is not set. To set the libc++ version: ./install_libcxx.sh -v X.Y.Z"
-    exit 4
-fi
+VERSION=${!OPTIND}
+OPTIND=$(($OPTIND + 1))
 
-if [ ${VERSION} == $TRUNK_VERSION ]; then
+PREFIX="${!OPTIND}"
+PREFIX="${PREFIX:-/usr}"
+
+case "$VERSION" in
+    3.4) VERSION=3.4.2;;
+    3.5) VERSION=3.5.2;;
+    3.6) VERSION=3.6.2;;
+    3.7) VERSION=3.7.1;;
+    3.8) VERSION=3.8.1;;
+    3 | 3.9) VERSION=3.9.1;;
+    4 | 4.0) VERSION=4.0.1;;
+esac
+
+VERSION_ARRAY=(${VERSION//./ })
+VERSION_MAJOR=${VERSION_ARRAY[0]}
+CMAKE_ARGS=""
+
+if [[ "$VERSION_MAJOR" -lt $TRUNK_VERSION ]]; then
+    echo "Fetching libc++ and libc++abi version: ${VERSION}..."
+    mkdir -p llvm-source/projects/libcxx llvm-source/projects/libcxxabi
+    LLVM_URL="http://releases.llvm.org/${VERSION}/llvm-${VERSION}.src.tar.xz"
+    LIBCXX_URL="http://releases.llvm.org/${VERSION}/libcxx-${VERSION}.src.tar.xz"
+    LIBCXXABI_URL="http://releases.llvm.org/${VERSION}/libcxxabi-${VERSION}.src.tar.xz"
+    curl -# $LIBCXXABI_URL | tar -xJf - -C llvm-source/projects/libcxxabi --strip-components=1 &
+    JOBS=$!
+    curl -# $LIBCXX_URL | tar -xJf - -C llvm-source/projects/libcxx --strip-components=1 &
+    JOBS+=" $!"
+    curl -# $LLVM_URL | tar -xJf - -C llvm-source --strip-components=1 &
+    JOBS+=" $!"
+else
     echo "Fetching libc++ and libc++abi tip-of-trunk..."
 
     # Checkout LLVM sources
     git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
-    git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
-    git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
-else
-    echo "Fetching libc++/libc++abi version: ${VERSION}..."
-    LLVM_URL="http://releases.llvm.org/${VERSION}/llvm-${VERSION}.src.tar.xz"
-    LIBCXX_URL="http://releases.llvm.org/${VERSION}/libcxx-${VERSION}.src.tar.xz"
-    LIBCXXABI_URL="http://releases.llvm.org/${VERSION}/libcxxabi-${VERSION}.src.tar.xz"
-    curl -O $LLVM_URL
-    curl -O $LIBCXX_URL
-    curl -O $LIBCXXABI_URL
-
-    mkdir llvm-source
-    mkdir llvm-source/projects
-    mkdir llvm-source/projects/libcxx
-    mkdir llvm-source/projects/libcxxabi
-
-    tar -xf llvm-${VERSION}.src.tar.xz -C llvm-source --strip-components=1
-    tar -xf libcxx-${VERSION}.src.tar.xz -C llvm-source/projects/libcxx --strip-components=1
-    tar -xf libcxxabi-${VERSION}.src.tar.xz -C llvm-source/projects/libcxxabi --strip-components=1
+    git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx &
+    JOBS=$!
+    git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi &
+    JOBS+=" $!"
 fi
 
 mkdir llvm-build
 cd llvm-build
+for pid in $JOBS; do wait -n; done
 
-# - libc++ versions < 4.x do not have the install-cxxabi and install-cxx targets
 # - only ASAN is enabled for clang/libc++ versions < 4.x
-if [[ $VERSION == *"3."* ]]; then
-    cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} \
-          -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
-          ../llvm-source
+if [[ $VERSION_MAJOR -lt 4 ]]; then
     if [[ $SANITIZER == "Address;Undefined" ]]; then
         ASAN_FLAGS="-fsanitize=address"
-        cmake -DCMAKE_CXX_FLAGS="${ASAN_FLAGS}" -DCMAKE_EXE_LINKER_FLAGS="${ASAN_FLAGS}" ../llvm-source
+        CMAKE_ARGS+=" -DCMAKE_CXX_FLAGS=$ASAN_FLAGS -DCMAKE_EXE_LINKER_FLAGS=$ASAN_FLAGS"
     fi
-    make cxx -j2 VERBOSE=1
-    sudo cp -r lib/* /usr/lib/
-    sudo cp -r include/c++ /usr/include/
 else
-    cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} \
-          -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
-          -DLIBCXX_ABI_UNSTABLE=ON \
-          -DLLVM_USE_SANITIZER=${SANITIZER} \
-          ../llvm-source
-    make cxx -j2 VERBOSE=1
-    sudo make install-cxxabi install-cxx
+    CMAKE_ARGS+=" -DLIBCXX_ABI_UNSTABLE=ON -DLLVM_USE_SANITIZER=$SANITIZER"
 fi
 
-exit 0
+cmake ../llvm-source -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="${PREFIX}" $CMAKE_ARGS
+make cxx -j2 VERBOSE=1
+
+# - libc++ versions < 4.x do not have the install-cxxabi and install-cxx targets
+if [[ $VERSION_MAJOR -lt 4 ]]; then
+    mkdir -p "${PREFIX}/lib/" "${PREFIX}/include/"
+    sudo cp -r lib/* "${PREFIX}/lib/"
+    sudo cp -r include/c++ "${PREFIX}/include/"
+else
+    sudo make install-cxxabi install-cxx
+fi
