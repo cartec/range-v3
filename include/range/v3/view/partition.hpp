cf4b88bf6feb81efdcfd37296afe099c3ea952d3
Casey Carter <Casey@Carter.net>
2018-05-18 22:22:28 -0700

    Implement `view::partition`

---
+++ include/range/v3/view/partition.hpp
@@ -0,0 +1,141 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Casey Carter 2018
+//  Copyright Eric Niebler 2018
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+#ifndef RANGE_V3_VIEW_PARTITION_HPP
+#define RANGE_V3_VIEW_PARTITION_HPP
+
+#include <tuple>
+#include <meta/meta.hpp>
+#include <range/v3/range_fwd.hpp>
+#include <range/v3/view_adaptor.hpp>
+#include <range/v3/utility/iterator_concepts.hpp>
+#include <range/v3/view/view.hpp>
+
+namespace ranges
+{
+    inline namespace v3
+    {
+        namespace detail
+        {
+            template<typename Rng, typename Out, typename Pred, typename Proj>
+            using PartitionConstraint = meta::and_<
+                InputRange<Rng>, WeaklyIncrementable<Out>, IndirectlyCopyable<iterator_t<Rng>, Out>,
+                IndirectPredicate<Pred &, projected<iterator_t<Rng>, Proj>>>;
+        }
+
+        template<typename Rng, typename Out, typename Pred, typename Proj = ident>
+        struct partition_view
+          : view_adaptor<
+                partition_view<Rng, Out, Pred, Proj>, Rng,
+                is_finite<Rng>::value ? finite : range_cardinality<Rng>::value>
+        {
+            friend range_access;
+
+            CONCEPT_ASSERT(detail::PartitionConstraint<Rng, Out, Pred, Proj>());
+
+            partition_view() = default;
+            RANGES_CXX14_CONSTEXPR partition_view(Rng rng, Out out, Pred pred, Proj proj = Proj{})
+              : partition_view::view_adaptor{std::move(rng)}
+              , data_(std::move(out), std::move(pred), std::move(proj))
+            {}
+
+        private:
+            struct adaptor : adaptor_base
+            {
+                using single_pass = std::true_type;
+
+                adaptor() = default;
+                adaptor(partition_view &pv)
+                  : pv_{&pv}
+                {}
+                iterator_t<Rng> begin(partition_view &pv)
+                {
+                    auto it = ranges::begin(pv.base());
+                    pv_->satisfy(it);
+                    return it;
+                }
+                void next(iterator_t<Rng> &it)
+                {
+                    RANGES_EXPECT(it != ranges::end(pv_->base()));
+                    pv_->satisfy(++it);
+                }
+                void prev() = delete;
+
+            private:
+                partition_view *pv_;
+            };
+
+            adaptor begin_adaptor()
+            {
+                return adaptor{*this};
+            }
+
+            void satisfy(iterator_t<Rng> &it)
+            {
+                auto &out = std::get<0>(data_);
+                auto &pred = std::get<1>(data_);
+                auto &proj = std::get<2>(data_);
+                for (auto const last = ranges::end(this->base()); it != last; ++it)
+                {
+                    auto &&value = *it;
+                    if (!invoke(pred, invoke(proj, value))) break;
+                    out = static_cast<decltype(value) &&>(value);
+                    ++out;
+                }
+            }
+
+            std::tuple<Out, semiregular_t<Pred>, semiregular_t<Proj>> data_{};
+        };
+
+        namespace view
+        {
+            struct partition_fn
+            {
+            private:
+                template<typename Out, typename Pred, typename Proj = ident,
+                    CONCEPT_REQUIRES_(WeaklyIncrementable<Out>() &&
+                        CopyConstructible<Pred>() && CopyConstructible<Proj>())>
+                static RANGES_CXX14_CONSTEXPR auto
+                bind(partition_fn fn, Out out, Pred pred, Proj proj = Proj{})
+                RANGES_DECLTYPE_AUTO_RETURN
+                (
+                    make_pipeable(std::bind(fn, std::placeholders::_1, out, pred, proj))
+                )
+            public:
+                template<typename Rng, typename Out, typename Pred, typename Proj = ident,
+                    CONCEPT_REQUIRES_(detail::PartitionConstraint<Rng, Out, Pred, Proj>())>
+                RANGES_CXX14_CONSTEXPR partition_view<all_t<Rng>, Out, Pred, Proj>
+                operator()(Rng &&rng, Out out, Pred pred, Proj proj = Proj{}) const
+                {
+                    return {all(static_cast<Rng &&>(rng)),
+                        std::move(out), std::move(pred), std::move(proj)};
+                }
+
+#ifndef RANGES_DOXYGEN_INVOKED
+                template<typename Rng, typename Out, typename Pred, typename Proj = ident,
+                    CONCEPT_REQUIRES_(!detail::PartitionConstraint<Rng, Out, Pred, Proj>())>
+                void operator()(Rng &&rng, Out out, Pred pred, Proj proj = Proj{}) const
+                {
+                    ; // FIXME
+                }
+#endif
+            };
+
+            /// \relates partition_fn
+            /// \ingroup group-views
+            RANGES_INLINE_VARIABLE(view<partition_fn>, partition)
+        } // namespace view
+    } // namespace v3
+} // namespace ranges
+
+#endif // RANGE_V3_VIEW_PARTITION_HPP
---
+++ test/view/partition.cpp
@@ -0,0 +1,34 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Casey Carter 2018
+//  Copyright Eric Niebler 2018
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+#include <range/v3/view/partition.hpp>
+#include "../simple_test.hpp"
+#include "../test_utils.hpp"
+
+int main()
+{
+    {
+        int some_ints[] = {0,1,2,3,4,5,6,7};
+        std::vector<int> vec1, vec2;
+        auto is_even = [](int i) { return i % 2 == 0; };
+        auto is_nearly_pow2 = [](int i) { return (i & (i + 1)) == 0; };
+        auto rng = some_ints |
+            ranges::view::partition(ranges::back_inserter(vec1), is_even) |
+            ranges::view::partition(ranges::back_inserter(vec2), is_nearly_pow2);
+        check_equal(rng,  {5});
+        check_equal(vec1, {0,2,4,6});
+        check_equal(vec2, {1,3,7});
+    }
+
+    return test_result();
+}
